1aa96d2715c7aaa26105e9a49f1d5d0265888966
myplc <plctohmi@gmail.com>
2026-02-23 11:22:37 +0900

    내용업

--- btc.py
+++ btc.py
@@ -4,4 +4,5 @@ url = "https://api4.binance.com/api/v3/ticker/price?symbol=BTCUSDT"
 res = req.get(url).text
 btc = res[29:34]
 won = str(int(btc)*1450)
-print( won[:1]+ '억 '+ won[1:5]+ '만 ' + won[5:] + '원') # -> 1억 1389만 6050원
+# print(won)
+print(((won[-12:-8] + "억") if won[-12:-8] else ""), won[-8:-4]+ '만',won[-4:]+ '원')
---
+++ sys3.py
@@ -0,0 +1,7 @@
+import sys
+args = sys.argv
+name = args[1]
+age = args[2]
+with open('./test_note.txt','a') as f:
+    f.write(name +" " + age+"\n")
+print("입력 되었습니다")
